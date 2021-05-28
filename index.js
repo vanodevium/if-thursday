bafb2235f954ee090b863f43df2317e9ed23c55a
Bohdan Shulha <bohdan@shulha.pp.ua>
2021-05-28 12:01:54 +0300

    improve extensibility

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 'use strict';
 
-const THURSDAY_DAY_OF_WEEK = 4
+const THURSDAY_DAY_OF_WEEK = 4;
+
+const defaultOptions = {
+  thursdayDayOfWeek: THURSDAY_DAY_OF_WEEK,
+  getCurrentDay,
+  dayComparator,
+}
 
 /**
  * Check if today is Thursday
  *
  * @return {boolean}
  */
-module.exports = function () {
-  return THURSDAY_DAY_OF_WEEK === new Date().getDay()
+module.exports = function (opts = {}) {
+  const options = withDefaultOptions(opts);
+  
+  const today = options.getCurrentDay();
+  const thursday = options.thursdayDayOfWeek;
+  const dayComparator = options.dayComparator;
+  
+  return dayComparator(thursday, today);
 };
+
+function getCurrentDay() {
+  return new Date().getDay();
+}
+
+function dayComparator(day, other) {
+  return day === other;
+}
+
+function withDefaultOptions(options) {
+  return { ...defaultOptions, ...options }
+}
